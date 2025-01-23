683ffe22b1cd91ac01efe797b7331995defc0266
Sebastián Veggiani <sebastian@exhibitsuite.com>
2025-01-22 21:12:18 -0300

    feat: site built

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+	username: "unentroponauta", // No leading @ here
+	homeLabel: "xol.ar",
+	homeUrl: "https://xol.ar",
 };
 
 data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
